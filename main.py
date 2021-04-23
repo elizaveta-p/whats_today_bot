8724d347e893cd7fd72175ff78bd44ed5e69d63d
elizaveta-p <75022621+elizaveta-p@users.noreply.github.com>
2021-04-23 21:58:36 +0300

    sending planned messages works

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import io
 
+import pytz
+from dateutil.tz import tzutc, tzlocal
+
 from pprint import pprint
 
 from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
@@ -27,57 +30,76 @@ def echo(update, context):
 def start(update, context):
     update.message.reply_text("Привет! Я......")
 
-# def remove_job_if_exists(name, context):
-#     """Удаляем задачу по имени.
-#     Возвращаем True если задача была успешно удалена."""
-#     current_jobs = context.job_queue.get_jobs_by_name(name)
-#     if not current_jobs:
-#         return False
-#     for job in current_jobs:
-#         job.schedule_removal()
-#     return True
+
+def remove_job_if_exists(name, context):
+    """Удаляем задачу по имени.
+    Возвращаем True если задача была успешно удалена."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
 
 
-# Обычный обработчик, как и те, которыми мы пользовались раньше.
-# def set_timer(update, context):
-#     """Добавляем задачу в очередь"""
-#     pass
-#
-#
-# def notify_me(update, context):
-#     chat_id = update.message.chat_id
-#     try:
-#         # args[0] должен содержать значение аргумента
-#         # (секунды таймера)
-#         time = int(context.args[0])
-#         if due < 0:
-#             update.message.reply_text(
-#                 'Извините, не умеем возвращаться в прошлое')
-#             return
-#
-#         # Добавляем задачу в очередь
-#         # и останавливаем предыдущую (если она была)
-#         job_removed = remove_job_if_exists(
-#             str(chat_id),
-#             context
-#         )
-#         context.job_queue.run_once(
-#             task,
-#             due,
-#             context=chat_id,
-#             name=str(chat_id)
-#         )
-#         text = f'Вернусь через {due} секунд!'
-#         if job_removed:
-#             text += ' Старая задача удалена.'
-#         # Присылаем сообщение о том, что всё получилось.
-#         update.message.reply_text(text)
-#
-#     except (IndexError, ValueError):
-#         update.message.reply_text('Использование: /set <секунд>')
+def notify_me(update, context):
+    chat_id = update.message.chat_id
+    try:
+        if context.args:
+            time = datetime.datetime.strptime(context.args[0], "%H:%M").time()
+        else:
+            time = datetime.datetime.strptime('00:00', "%H:%M").time()
+
+        job_removed = remove_job_if_exists(
+            str(chat_id),
+            context
+        )
+        local_timezone = pytz.timezone('Europe/Moscow')
+        target_time = datetime.time(hour=time.hour,minute=time.minute).replace(tzinfo=local_timezone)
+        # print(target_time)
+        context.job_queue.run_daily(
+            callback=send_holidays,
+            context=chat_id,
+            days=(0, 1, 2, 3, 4, 5, 6),
+            time=target_time,
+            name=str(chat_id))
+        text = f'Оповещения будут приходить в {time:%H:%M} каждый день.'
+        if job_removed:
+            text += ' Старая задача удалена.'
+        # Присылаем сообщение о том, что всё получилось.
+        update.message.reply_text(text)
+    except (IndexError, ValueError):
+        update.message.reply_text('Использование: /set <секунд>')
+
+
+def dont_notify_me(update, context):
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Оповещения больше приходить не будут.' if job_removed else 'Оповещения не были установлены.'
+    update.message.reply_text(text)
+
+
+def send_holidays(context):
+    job = context.job
+    date = datetime.date.today()
+    # print(f"here at time {datetime.datetime.now().time()}")
+    # chat_id = context.message.chat_id
+    # job = context.job
+    # pprint(job)
+    holiday = get_holidays(date)
+    print(holiday)
+    # my_str = [f"{x[1][0]} в {x[1][1]}" for x in enumerate(holiday)]
+    keyboard = [[InlineKeyboardButton(text=str(x[0] + 1), callback_data=x[1]['en']) for x in enumerate(holiday)]]
+    markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    # context.message.reply_text(text=make_readable(holiday, date), reply_markup=markup)
+    # context.bot.send_message(chat_id=job.context, text=make_readable(holiday, date), reply_markup=markup)
+    context.bot.send_message(job.context, text=make_readable(holiday, date), reply_markup=markup)
+
+    # update.message.reply_text(make_readable(holiday, date), reply_markup=markup)
 
 
 def holidays(update, context):
+    print(context)
     update.message.reply_text('Загружаю праздники...')
     if context.args:
         date = datetime.datetime.strptime(context.args[0], '%d-%m-%Y').date()  # ValueError если не подходит под формат
@@ -144,6 +166,12 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("countries", countries))
     dp.add_handler(CommandHandler("holidays", holidays))
+    dp.add_handler(CommandHandler("notify_me", notify_me,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("dont_notify_me", dont_notify_me,
+                                  pass_chat_data=True))
 
     dp.add_handler(text_handler)
 
